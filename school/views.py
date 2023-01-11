56a5f2bdfaa7f82d9cd64221cd9d2b4277b5af4e
ndolocr <crndolo@gmail.com>
2023-01-11 17:04:02 +0300

    Creating dummy data for posting school  data

--- school/views.py
+++ school/views.py
@@ -6,6 +6,7 @@ from py2neo import Graph
 
 def save_data(request):
     graph = Graph(**settings.NEO4J_DATABASES['default'])
-    data = request.POST.get('data')
+    # data = request.POST.get('data')
+    data = "Ndolo"
     graph.run("CREATE (:Label {data: {data}})", data=data)
     return HttpResponse("Data saved successfully.")
