d55bc3a386eda1ff4649b50d4b3e5060ced76109
ndolocr <crndolo@gmail.com>
2023-01-11 15:02:51 +0300

    Include school for field in schoo model

--- school/models.py
+++ school/models.py
@@ -84,10 +84,10 @@ class School(models.Model):
     
     name = models.CharField(max_length=255, null=False)
     location = models.CharField(max_length=255, null=True)
-    school_for = models.CharField(max_length=255, choices=SCHOOL_FOR_CHOICES, null=False)
     school_type = models.CharField(max_length=255, choices=SCHOOL_TYPE_CHOICES, null=False)
     school_category = models.CharField(max_length=255, choices=SCHOOL_CATEGORY_CHOICES, null=False)
     county = models.CharField(max_length=255, choices=COUNRY_CHOICES, null=False, default="Mombasa")
+    school_for = models.CharField(max_length=255, choices=SCHOOL_FOR_CHOICES, null=False, default="Mixed")
 
     updated_on = models.DateTimeField( auto_now = True)
     created_on = models.DateTimeField( auto_now_add = True)
